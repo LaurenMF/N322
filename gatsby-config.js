4af108069dd72159b315c776b795b9e1acbbecce
LaurenMF <57876652+LaurenMF@users.noreply.github.com>
2021-09-29 12:19:38 -0400

    Homework 2

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~lfasig/n322/todo-list",
+    title: "EllF Todo List",
+  },
+  plugins: [],
+  pathPrefix: '/~lfasig/todo-list'
+};
